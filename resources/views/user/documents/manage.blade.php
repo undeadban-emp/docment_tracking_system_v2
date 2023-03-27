c265781ce7fdcf7df4ca1b8c8ffc6f7193e4abb2
eDcel <undeadban.emp@gmail.com>
2023-03-27 14:30:12 +0800

    fix url

--- resources/views/user/documents/manage.blade.php
+++ resources/views/user/documents/manage.blade.php
@@ -24,7 +24,7 @@
                               <td class='text-dark fw-medium font-size-14'>{{ $document->forwarded_by_user->fullname }}</td>
                               <td class='text-dark fw-medium font-size-14'>{{ $document->avail_by->fullname }}</td>
                               <td class='text-center'>
-                                   <a href="{{ url('/received/service/{{ $document->tracking_number }}') }}" class='btn btn-primary'>
+                                   <a href="{{ url('/received/service/'.$document->tracking_number) }}" class='btn btn-primary'>
                                         VIEW
                                    </a>
                               </td>
